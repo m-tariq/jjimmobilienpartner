02134cf0790e9523f48760cbd1cc05cbef350706
Tariq <muhammadtariq9619@gmail.com>
2024-11-09 01:35:02 +0500

    add header and footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer>
+      {/* <p>© 2023 jjimmobilienpartner. All rights reserved.</p> */}
+    </footer>
+  );
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Header() {
+  return <header>{/* <h1>Header</h1> */}</header>;
+}
