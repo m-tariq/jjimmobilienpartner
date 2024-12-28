c853aa156ad70c7307ae2351c7aecf84277da57c
tariq <muhammadtariq9619@gmail.com>
2024-12-28 17:15:19 +0500

    build refresh

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -183,7 +183,7 @@ export default function LandingPage() {
             Hier gelangen Sie zu unserem Kontaktformular, wo Sie uns unkompliziertund unverbindlich die Kerndaten zu Ihrem Objekt übermitteln können.
           </p>
           <div className='d-flex gap-5 justify-content-center flex-wrap'>
-          <Link href="/contact" className="btn bg-white px-5 fs-6 fw-600 font-raleway rounded-1">
+          <Link href="/contact-us" className="btn bg-white px-5 fs-6 fw-600 font-raleway rounded-1">
             Kontakt
           </Link>
           <Link href="" className="btn bg-white px-5 fs-6 fw-600 font-raleway rounded-1">
